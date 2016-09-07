53722c86f3c93035944c46e103003c01d63d0ea8
Ryan Becwar <rbecwar1@gmail.com>
2016-09-07 10:59:35 -0400

    building out regret and testing

--- regret_matching.cpp
+++ regret_matching.cpp
@@ -12,7 +12,28 @@ To evaluate this, best fit line is drawn (soon to be expanded to a quadratic)
 
 class regret_projection {
 public:
+  void setConnections(std::vector<double> values){
+    std::sort(values.begin(), values.end(), std::greater<double>());
+    connectionValues = values;
+  }
 
-  vector<double> connectionValues;
+  void findProjection(){
+    const double weightDecayRate = 0.9;
+    double weight = 1.0;
+    double weightDivisor = 0, sum = 0;
+    for(unsigned int i=1; i<connectionValues.size(); i++){
+      double pointSlope = (connectionValues[i] - connectionValues[0]) / i;
+      sum += pointSlope * weight;
+      weightDivisor += weight;
+    }
 
+    projectionSlope = sum / weightDivisor;
+  }
+
+  std::vector<double> connectionValues;
+  double projectionSlope;
+};
+
+int main(){
+  return 0;
 }
